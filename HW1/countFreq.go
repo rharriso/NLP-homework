b33db8e956c8a6cc3883fd66b33e56745ef4afac
rharriso <rtharrison86@gmail.com>
2015-08-31 22:33:37 -0400

    emission counts

--- HW1/countFreq.go
+++ HW1/countFreq.go
@@ -7,15 +7,21 @@ import (
 	"strings"
 )
 
+var wordCount map[string]int
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("\n Usage: countFreq path/to/inputFile \n")
 		return
 	}
+
+	// open the passed file
 	file, err := os.Open(os.Args[1])
 	if err != nil {
 		panic(err.Error())
 	}
+
+	wordCount = make(map[string]int)
 
 	reader := bufio.NewReader(file)
 	scanner := bufio.NewScanner(reader)
@@ -24,7 +30,12 @@ func main() {
 	for scanner.Scan() {
 		members := strings.SplitAfter(scanner.Text(), " ")
 		if len(members) == 2 {
-			fmt.Printf("%s \t aahhahah \t %s\n", members[0], members[1])
+			wordCount[members[0]]++
 		}
+	}
+
+	// print word counts
+	for word, count := range wordCount {
+		fmt.Println(word, count)
 	}
 }
